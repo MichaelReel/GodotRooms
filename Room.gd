09f5b53a49814ecdd79ff8ccbdebb4700651883c
Michael Reel <mykilr@gmail.com>
2017-12-01 16:42:31 +0000

    Implemented a very simple maze for tilemap. Will extract out the maze creation for other uses later.

--- Room.gd
+++ Room.gd
@@ -6,6 +6,11 @@ var room_size
 
 var base_map
 
+var path_length = 2
+
+var frontier = []
+var dirs = [Vector2(-path_length, 0), Vector2(path_length, 0), Vector2(0, -path_length), Vector2(0, path_length)]
+
 func init_map():
 	var map = TileMap.new()
 	
@@ -16,7 +21,7 @@ func init_map():
 	
 	return map
 
-func _init(resource, gen_seed = 1):
+func _init(resource, gen_seed = OS.get_time().second):
 	self.resource = resource
 	self.tile_size = resource.tile_size
 	self.room_size = resource.average_room_size
@@ -28,46 +33,55 @@ func _init(resource, gen_seed = 1):
 func generate_content():
 	self.room_create()
 
-func room_create():
+func set_all_tiles(tile):
 	for tile_y in range(self.room_size.y):
 		for tile_x in range(self.room_size.x):
-			# Start with no walls
-			self.base_map.set_cell(tile_x, tile_y, resource.walls[0])
-	var border = Rect2(0,0,self.room_size.x - 1,self.room_size.y - 1)
-	for bounds in random_rect_breakdown(border):
-		var rand = randi()%3
-		if rand < 2:
-			draw_rect_feature(self.base_map, bounds, (rand == 1))
-	draw_rect_feature(self.base_map, border, true)
+			self.base_map.set_cell(tile_x, tile_y, tile)
 
-func draw_rect_feature(room, bounds, dropped = true):
-	room.set_cell(bounds.pos.x, bounds.pos.y, resource.walls[14 if dropped else 01])
-	room.set_cell(bounds.pos.x, bounds.end.y, resource.walls[11 if dropped else 04])
-	room.set_cell(bounds.end.x, bounds.pos.y, resource.walls[13 if dropped else 02])
-	room.set_cell(bounds.end.x, bounds.end.y, resource.walls[07 if dropped else 08])
-	for tile_y in range(bounds.pos.y + 1, bounds.end.y):
-		room.set_cell(bounds.pos.x, tile_y, resource.walls[10 if dropped else 05])
-		room.set_cell(bounds.end.x, tile_y, resource.walls[05 if dropped else 10])
-	for tile_x in range(bounds.pos.x + 1, bounds.end.x):
-		room.set_cell(tile_x, bounds.pos.y, resource.walls[12 if dropped else 03])
-		room.set_cell(tile_x, bounds.end.y, resource.walls[03 if dropped else 12])
+func room_create():
+	# Start with no walls
+	set_all_tiles(resource.walls[1])
+	prims_algorithm(resource.walls[0])
 
-func random_rect_breakdown(bounds, choice = 1):
-	var bounds_list = [bounds]
-	# if any of the bounds are too small already, don't split further
-	if bounds.size.x > 5 and bounds.size.y > 5:
-#		# Split, Horizontal (1) or vertical (2), or don't split at all (0)
-#		var choice = randi()%2 + 1 # %3
-		if choice == 1:
-			var split_x_line = randi() % int(bounds.size.x - 4) + 2
-			bounds_list += random_rect_breakdown(Rect2(bounds.pos.x + 1, bounds.pos.y + 1, split_x_line - 1, bounds.size.y - 2), 2)
-			bounds_list += random_rect_breakdown(Rect2(bounds.pos.x + split_x_line + 1, bounds.pos.y + 1, bounds.size.x - split_x_line - 2, bounds.size.y  - 2), 2)
-		elif choice == 2:
-			var split_y_line = randi() % int(bounds.size.y - 4) + 2
-			bounds_list += random_rect_breakdown(Rect2(bounds.pos.x + 1, bounds.pos.y + 1, bounds.size.x - 2, split_y_line - 1), 1)
-			bounds_list += random_rect_breakdown(Rect2(bounds.pos.x + 1, bounds.pos.y + split_y_line + 1, bounds.size.x - 2, bounds.size.y - split_y_line - 2), 1)
-	
-	return bounds_list
+func prims_algorithm(tile):
+	var start_x = (randi() % int(self.room_size.x / self.path_length)) * self.path_length + int(self.path_length / 2)
+	var start_y = (randi() % int(self.room_size.y / self.path_length)) * self.path_length + int(self.path_length / 2)
+	var pos = Vector2(start_x, start_y)
+	self.base_map.set_cellv(pos, tile)
+	add_frontiers(pos, tile)
+	while frontier.size() > 0:
+		var ind = randi() % frontier.size()
+		visit(frontier[ind], tile)
+		add_frontiers(frontier[ind], tile)
+		frontier.remove(ind)
+
+func add_frontiers(pos, tile):
+	for dpos in get_dirs_rand_order():
+		var new_pos = pos + dpos
+		var cell_at = self.base_map.get_cellv(new_pos)
+		if cell_at != TileMap.INVALID_CELL && cell_at != tile && not frontier.has(new_pos):
+			frontier.append(new_pos)
+
+func get_dirs_rand_order():
+	var dirs = [] + self.dirs
+	var new_dirs = []
+	while dirs.size()  > 0:
+		var ind = randi() % dirs.size()
+		new_dirs.append(dirs[ind])
+		dirs.remove(ind)
+	return new_dirs
+
+func visit(pos, tile):
+	self.base_map.set_cellv(pos, tile)
+	for dpos in get_dirs_rand_order():
+		var new_pos = pos + dpos
+		var cell_at = self.base_map.get_cellv(new_pos)
+		if cell_at == tile:
+			# draw path to cell and stop
+			while (new_pos - pos).length() >= 1:
+				self.base_map.set_cellv(new_pos, tile)
+				new_pos -= dpos.normalized()
+			return
 
 func _exit_tree():
 	self.base_map.queue_free();
